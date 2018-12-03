1f96f83cd040afab97eb1da84c43ddb000eb187d
Kasper Siig <kasp9250@easv365.dk>
2018-12-03 20:18:16 +0100

    nope

--- PetShop.RestApi/Program.cs
+++ PetShop.RestApi/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -14,7 +15,19 @@ namespace PetShop.RestApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Loopback, 5000);  // http:localhost:5000
+                    options.Listen(IPAddress.Any, 80);         // http:*:80
+                    options.Listen(IPAddress.Loopback, 443, listenOptions =>
+                    {
+                        listenOptions.UseHttps("petshop.pfx", "test");
+                    });
+                }).UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
